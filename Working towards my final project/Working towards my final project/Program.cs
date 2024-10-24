37c3b0e634e81d5cad14ed8f5b8d0777a317fafc
Assem130 <assem99mustafa@gmail.com>
2024-10-24 23:11:52 +0200

    update push code

---
+++ Working towards my final project/Working towards my final project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Working_towards_my_final_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
